d638643f6862835758af8972266eeecc8e328620
Ayush Kumar Nath <n.ayushknath@gmail.com>
2024-01-21 19:36:56 +0530

    Removes localStorage theme saver

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 const Layout = () => {
-  const [theme, setTheme] = useState(
-    JSON.parse(localStorage.getItem("rest-countries-api-theme")) || "light"
-  );
+  const [theme, setTheme] = useState("light");
 
   function changeTheme() {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   }
-
-  useEffect(() => {
-    localStorage.setItem("rest-countries-api-theme", JSON.stringify(theme));
-  }, [theme]);
 
   return (
     <div
